0ce265c6d006c2f6769d64f32c0185bc51500167
wira885 <laluilhamwira@gmail.com>
2024-11-17 15:19:19 +0800

    Add files via upload

---
+++ wira-alprog-tugas6.py
@@ -0,0 +1,12 @@
+#luas segitiga prog 2
+a = 21
+t = 24
+l = 252,0
+
+#casting tipe data
+a = int(input("masukkan nilai a:"))
+print (" nilai a adalah : ", a, type(a))
+t = int(input("masukkan nilai t:"))
+print (" nilai t adalah : ", t, type(t))
+l = float(input("masukkan nilai l:"))
+print (" nilai l adalah : ", l, type(l))
