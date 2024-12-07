94fa1367b148466e1eba705321dedf4c2017d9a1
wira885 <laluilhamwira@gmail.com>
2024-12-07 14:23:26 +0800

    Add files via upload

---
+++ tugaswira 8.py
@@ -0,0 +1,30 @@
+a = 24
+b = 21
+
+#operasi kali X
+hasil = a * b 
+print("nilai dari a * b =",hasil)
+
+#operasi bagi :
+hasil = a / b 
+print("nilai dari a / b =",hasil)
+
+#operasi tambah +
+hasil = a + b 
+print("nilai dari a + b =",hasil)
+
+#operasi kurang -
+hasil = a - b 
+print("nilai dari a - b=",hasil)
+
+#operasi perpangkatan **
+hasil = a ** b 
+print("nilai dari a ** b=",hasil)
+
+#modulus (sisa bagi) %
+hasil = a % b 
+print("nilai dari a mod b=",hasil)
+
+#floor dividion //
+hasil = a // b 
+print("nilai dari a flor b=",hasil)
