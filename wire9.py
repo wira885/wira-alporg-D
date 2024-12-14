fffdc106420444c54de8b8c5376841ac640cfe01
wira885 <laluilhamwira@gmail.com>
2024-12-14 19:43:02 +0800

    Add files via upload

---
+++ wire9.py
@@ -0,0 +1,17 @@
+# suhu dalam celcius = 21
+# suhu dalam fahrenheit =
+# suhu dalam kelvin = 
+# suhu dalam reamur =
+
+# suhu dalam celcius
+celcius = 24
+
+# rumus koversi suhu
+farenheit = (celcius * 9/5) + 32
+kelvin = celcius + 273.15
+reamur = celcius * 4/5
+
+# cetak nilai suhu
+print("suhu dalam fahrenheit:", farenheit )
+print("suhu dalam kelvin:", kelvin)
+print("suhu dalam reamur:", reamur)
