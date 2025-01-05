2fcb7ba5cacbc97b7f0cb084d8ad13c59dc8b8c8
wira885 <laluilhamwira@gmail.com>
2025-01-05 15:40:48 +0800

    Add files via upload

---
+++ tugas10.py
@@ -0,0 +1,26 @@
+# perbandingan - jawaban ada 2 ture false
+
+# >,  <, >=,  <=, ==,  !=, is, isnot
+
+x = 5
+
+hasil = x > 6
+print("x > 6 = ", hasil)
+
+hasil = x < 6
+print("x < 6 = ", hasil)
+
+hasil = x >= 6
+print("x >= 6 = ", hasil)
+
+hasil = x <= 6
+print("x <= 6 = ", hasil)
+
+hasil = x == 6
+print("x == 6 = ", hasil)
+
+hasil = x != 6
+print("x != 6 = ", hasil)
+
+hasil = x  is 6
+print("x  is 6 = ", hasil)
